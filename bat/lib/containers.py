40b8826d5f9497557a99594905b34d19090087df
longgeek <longgeek@thstack.com>
2014-12-05 18:20:54 +0800

    Add exec pause unpause bp
    Change-Id: I1f38e94029462358ce04906ee5465cfdc6d08e8a

--- bat/lib/containers.py
+++ bat/lib/containers.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Longgeek <longgeek@gmail.com>
 
-from time import strftime
-from time import localtime
+import simplejson
 
 from bat.lib.sing_leton import DockerSingLeton
 
@@ -14,7 +13,7 @@ class Container_Manager(object):
     def __init__(self):
         self.connection = DockerSingLeton()
 
-    def _containers(self, cid=None):
+    def _containers(self, db_id, cid=None):
         """获取所有 Container 的信息
         # docker ps -a
         # docker ps -a | grep $cid
@@ -31,9 +30,9 @@ class Container_Manager(object):
                 return (0, '', all_containers)
 
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': db_id}, '')
 
-    def _inspect_container(self, cid):
+    def _inspect_container(self, db_id, cid):
         """获取一个 Container 的信息
         # docker inspect $cid
 
@@ -43,7 +42,7 @@ class Container_Manager(object):
             container = self.connection.inspect_container(cid)
             return (0, '', container)
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': db_id}, '')
 
     def create_container(self, msg):
         """创建 Container"""
@@ -66,20 +65,18 @@ class Container_Manager(object):
                 msg['cid'] = c_id
 
                 # 调用 _containers, 拿到 size status command created
-                containers = self._containers(c_id)
+                containers = self._containers(msg['id'], c_id)
                 if containers[0] == 0:
                     msg['size'] = containers[2]['SizeRootFs']
-                    msg['status'] = containers[2]['Status']
                     msg['command'] = containers[2]['Command']
-                    msg['created'] = strftime(
-                        '%Y-%m-%d %X', localtime(containers[2]['Created']))
+                    msg['created'] = containers[2]['Created']
                 else:
                     return containers
 
                 # 调用 _inspect_c, 拿到 name hostname
-                inspect_c = self._inspect_container(c_id)
+                inspect_c = self._inspect_container(msg['id'], c_id)
                 if inspect_c[0] == 0:
-                    msg['name'] = inspect_c[2]['Name'][1:]
+                    msg['name'] = inspect_c[2]['Name']
                     msg['hostname'] = inspect_c[2]['Config']['Hostname']
                 else:
                     return inspect_c
@@ -89,9 +86,10 @@ class Container_Manager(object):
                 return start_c
 
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': msg['id'],
+                        'message_type': 'create_container'}, '')
 
-    def start_container(self, msg, c_id=None):
+    def start_container(self, msg, c_id=None, publish_all_ports=True):
         """启动 Container"""
 
         if not msg['cid']:
@@ -100,11 +98,17 @@ class Container_Manager(object):
             container_id = msg['cid']
 
         try:
-            self.connection.start(container=container_id)
+            self.connection.start(container=container_id,
+                                  publish_all_ports=True)
+            containers = self._containers(msg['id'], container_id)
+            if containers[0] == 0:
+                msg['status'] = containers[2]['Status']
+            else:
+                return containers
             return (0, '', msg)
 
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': msg['id']}, '')
 
     def stop_container(self, msg, timeout=5):
         """停止 Container"""
@@ -113,13 +117,13 @@ class Container_Manager(object):
             self.connection.stop(c_id, timeout)
 
             # 调用 _containers, 拿到 status
-            containers = self._containers(c_id)
+            containers = self._containers(msg['id'], c_id)
             if containers[0] == 0:
                 msg['status'] = containers[2]['Status']
             return (0, '', msg)
 
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': msg['id']}, '')
 
     def restart_container(self, msg, timeout=5):
         """重启 Container"""
@@ -128,26 +132,74 @@ class Container_Manager(object):
             self.connection.restart(c_id, timeout)
 
             # 调用 _containers, 拿到 status
-            containers = self._containers(c_id)
+            containers = self._containers(msg['id'], c_id)
             if containers[0] == 0:
                 msg['status'] = containers[2]['Status']
             return (0, '', msg)
 
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': msg['id']}, '')
 
     def delete_container(self, msg):
         """删除 Container"""
         try:
-            self.connection.remove_container(container=msg['cid'],
-                                             force=True)
+            # 调用 _inspect_c, 判断容器是否暂停
+            inspect_c = self._inspect_container(msg['id'], msg['cid'])
+            if inspect_c[0] == 0:
+                if inspect_c[2]['State']['Paused']:
+                    unpause_result = self.unpause_container(msg)
+                    if unpause_result[0] != 0:
+                        return unpause_result
+                self.connection.remove_container(container=msg['cid'],
+                                                 force=True)
+            else:
+                return inspect_c
             return (0, '', msg)
         except Exception, e:
-            return (1, e, '')
+            return (1, {'error': str(e), 'id': msg['id']}, '')
 
     def exec_container(self, msg):
         """增加进程 Container"""
-        pass
+        try:
+            c_id = msg['cid']
+            c_list = simplejson.loads(msg['command'])
+            for c in c_list:
+                self.connection.execute(container=c_id, cmd=c,
+                                        detach=True, tty=True,
+                                        stderr=False, stdout=False)
+            return (0, '', msg)
+        except Exception, e:
+            return (1, {'error': str(e), 'id': msg['id']}, '')
+
+    def pause_container(self, msg):
+        """暂停容器"""
+        try:
+            c_id = msg['cid']
+            self.connection.pause(c_id)
+
+            # 调用 _containers, 拿到 status
+            containers = self._containers(msg['id'], c_id)
+            if containers[0] == 0:
+                msg['status'] = containers[2]['Status']
+            return (0, '', msg)
+
+        except Exception, e:
+            return (1, {'error': str(e), 'id': msg['id']}, '')
+
+    def unpause_container(self, msg):
+        """恢复暂停的容器"""
+        try:
+            c_id = msg['cid']
+            self.connection.unpause(c_id)
+
+            # 调用 _containers, 拿到 status
+            containers = self._containers(msg['id'], c_id)
+            if containers[0] == 0:
+                msg['status'] = containers[2]['Status']
+            return (0, '', msg)
+
+        except Exception, e:
+            return (1, {'error': str(e), 'id': msg['id']}, '')
 
 
 def main(msg):
@@ -156,5 +208,5 @@ def main(msg):
 
     """
     msg_type = msg['message_type']
-    tt = Container_Manager()
-    return getattr(tt, msg_type)(msg)
+    exec_action = Container_Manager()
+    return getattr(exec_action, msg_type)(msg)
